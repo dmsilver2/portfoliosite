1cda66bfc93a1d33c6a0d37bd4683e3a2d313d58
unknown <dmsilver2@gmail.com>
2018-06-18 10:32:59 -0400

    Added Sticky Nav Bar and did other minor changes nav bar css

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,14 +7,42 @@ import Projects from './Projects';
 import Contact from './Contact';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      position: 0,
+      screenWidth:0,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+    window.addEventListener('resize', this.handleResize.bind(this));
+
+    this.handleResize();
+  }
+
+  handleScroll(event) {
+      let offset = event.srcElement.body.getBoundingClientRect().top;
+
+      this.setState({
+      position: offset,
+      });
+  }
+  handleResize(event) {
+    this.setState({
+    screenWidth: window.innerWidth,
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <Projects />
+        <Projects offset={this.state.position} />
         <div className="clear-fix" />
         <Contact />
-        <Navigation />
+        <Navigation screenWidth={this.state.screenWidth} offset={this.state.position} />
         {/* <footer>Created by Daniel Silver</footer> */}
       </div>
     );
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -4,19 +4,36 @@ import '../scss/Navigation.css';
 class Navigation extends Component {
   constructor() {
     super();
-    this.state = {
-      location: 0,
-    };
+  }
 
+  //Added Scroll and Resize events because when the nav bar is in fixed position,
+  //the bar is fixed relative to the viewport rather then the parent div container.
+  //Had to dinamically change the width.
+  componentDidMount() {
+    this.navBar = document.getElementById("navBar");
+    this.navBarParent = this.navBar.parentElement;
+
+    window.addEventListener('scroll', this.handleResizeScroll.bind(this));
+    window.addEventListener('resize', this.handleResizeScroll.bind(this));
+    this.handleResizeScroll();
+  }
+
+  handleResizeScroll(event) {
+    this.navBar.style.width = this.navBarParent.offsetWidth +"px";
   }
 
   render() {
+    let navClass = "navigation-bar";
+
+    if(this.props.offset<-605 && this.props.screenWidth>768)
+      navClass+=" navigation-sticky";
+
     return (
-      <div className="navigation-bar">
+      <div className={navClass} id="navBar">
         <nav>
           <a href="#daniel">Daniel Silver</a>
           <a href="#projects" >Projects</a>
-          <a href="#contact" >Contact</a>
+          <a className="no-right-border" href="#contact" >Contact</a>
         </nav>
       </div>
     );
